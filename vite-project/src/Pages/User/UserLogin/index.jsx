1e9c86a4419ec6a47c0f558d94ec51d6b351e42b
Kenko <kerimlikenan00@gmail.com>
2025-06-26 22:21:24 +0400

    eye done

--- vite-project/src/Pages/User/UserLogin/index.jsx
+++ vite-project/src/Pages/User/UserLogin/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { RiFireFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
-import { FaEyeSlash } from "react-icons/fa";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+
 function index() {
+  const [showPassword, setShowPassword] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -24,6 +27,7 @@ function index() {
       alert(`Email: ${values.email}\nPassword: ${values.password}`);
     },
   });
+
   return (
     <section id='loginbar'>
       <div className="overlay">
@@ -32,43 +36,46 @@ function index() {
             <div className="lb-head">EDUTERIA <RiFireFill/></div>
             <p>ADMIN</p>
           </div>
-           <form onSubmit={formik.handleSubmit}>
-      <div className='inputer'>
-        <input
-          placeholder='Email'
-          type="email"
-          name="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-     
-        <MdEmail/>
-           {formik.touched.email && formik.errors.email ? (
-          <div style={{ color: "red" }}>{formik.errors.email}</div>
-        ) : null}
-      </div>
-      
-      <div className='inputer'>
-        <input
-          placeholder='Password'
-          type="password"
-          name="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-       
-          <FaEyeSlash className='see'/>
-           {formik.touched.password && formik.errors.password ? (
-          <div style={{ color: "red" }}>{formik.errors.password}</div>
-        ) : null}
-                <NavLink to="/register" >You don't have account?</NavLink>
-      </div>
+          <form onSubmit={formik.handleSubmit}>
+            <div className='inputer'>
+              <input
+                placeholder='Email'
+                type="email"
+                name="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              <MdEmail/>
+              {formik.touched.email && formik.errors.email ? (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              ) : null}
+            </div>
 
-      <button type="submit">LOGIN</button>
-    </form>
-    <div className="loginfooter"></div>
+            <div className='inputer' style={{ position: "relative" }}>
+              <input
+                placeholder='Password'
+                type={showPassword ? "text" : "password"}
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              <span 
+                onClick={() => setShowPassword(!showPassword)} 
+                style={{ cursor: "pointer", position: "absolute", right: 10, top: "0%", transform: "translateY(-50%)" }}
+              >
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
+              </span>
+              {formik.touched.password && formik.errors.password ? (
+                <div style={{ color: "red" }}>{formik.errors.password}</div>
+              ) : null}
+              <NavLink to="/register" >You don't have account?</NavLink>
+            </div>
+
+            <button type="submit">LOGIN</button>
+          </form>
+          <div className="loginfooter"></div>
         </div>
       </div>
     </section>
